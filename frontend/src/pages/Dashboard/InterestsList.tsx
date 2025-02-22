3bc8b64e1e9ec27349d4f3a286db0ed77e5b6d0d
ImDarkTom <darktomg21@gmail.com>
2025-02-22 17:40:35 +0000

    add interest list to dashboard

--- frontend/src/pages/Dashboard/InterestsList.tsx
+++ frontend/src/pages/Dashboard/InterestsList.tsx
@@ -1,13 +1,16 @@
+import { IoMdClose } from 'react-icons/io';
+
 interface InterestsListProps {
 	list: string[];
+	onRemoveItem: (item: string) => void
 }
 
-function InterestsList({list}: InterestsListProps) {
+function InterestsList({list, onRemoveItem}: InterestsListProps) {
 	const renderedList = list.map((item) => {
-		return <div className="border-[1px] border-white p-2 w-fit rounded-xl">{item}</div>;
+		return <div className="border-[1px] border-white p-2 w-fit rounded-xl flex flex-row items-center gap-2">{item}<IoMdClose className="cursor-pointer" onClick={() => onRemoveItem(item)} /></div>;
 	});
 
-	return <div className="flex flex-row flex-wrap gap-2 my-">
+	return <div className="flex flex-row flex-wrap gap-2 my-2">
 		{renderedList}
 	</div>;
 }
