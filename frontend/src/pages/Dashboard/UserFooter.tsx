ffb85e66a8b164fdc7c6e6f062e52e2b6b2c6c09
Vojtech Baudys <vojta@baudys.name>
2025-02-22 18:31:57 +0000

    axios

--- frontend/src/pages/Dashboard/UserFooter.tsx
+++ frontend/src/pages/Dashboard/UserFooter.tsx
@@ -1,4 +1,5 @@
 import { User } from '@/types/user';
+import { googleLogout } from '@react-oauth/google';
 
 interface UserFooterProps {
     user: User;
@@ -8,7 +9,7 @@ function UserFooter({user}: UserFooterProps) {
 	return (
 		<div className=' flex flex-row items-center justify-between h-20 bg-gray-600 rounded-xl p-4'>
 			<img src={user.pfpUrl} className='h-full rounded-full' alt='User profile picture'></img>
-			<input type="button" value="Sign Out" className='h-full border-[1px] border-white rounded-xl ml-4 px-4 cursor-pointer' />
+			<input type="button" value="Sign Out" onClick={() => googleLogout()} className='h-full border-[1px] border-white rounded-xl ml-4 px-4 cursor-pointer' />
 		</div>
 	);
 }
