8e4364d54c000b56b82c0fd7ade8eb211290fa50
ImDarkTom <darktomg21@gmail.com>
2025-02-22 23:54:33 +0000

    add loadign skeletons for sidebat items

--- frontend/src/pages/Dashboard/UploadsList.tsx
+++ frontend/src/pages/Dashboard/UploadsList.tsx
@@ -1,5 +1,6 @@
 import { JSX, useEffect, useState } from 'react';
 import { Link } from 'react-router';
+import UploadListItemsSkeleton from './UploadListItemSkeleon';
 
 interface UploadListProps {
 	uploads: {
@@ -16,17 +17,19 @@ interface UploadListProps {
 
 const UploadsList = ({uploads}: UploadListProps) => {
 	const [renderedList, setRenderedList] = useState<JSX.Element[] | null>(null);
+	const [loaded, setLoaded] =  useState(false);
 
 	useEffect(() => {
 		setRenderedList(uploads.map((item) => {
 			const createdDateObject = new Date(item.createdAt);
 			const dateTimeString = `${createdDateObject.getHours()}:${createdDateObject.getMinutes()}, ${new Date(item.createdAt).toLocaleDateString()}`;
+			setLoaded(true);
 
 			return <Link
 				to={{
 					pathname: `/summary/${item._id}`
 				}}
-				className="flex flex-col w-full p-4 border-b-[1px] border-white hover:bg-gray-600 transition-colors duration-150">
+				className="flex flex-col w-full p-4 border-b-[1px] border-white last:border-0 hover:bg-gray-600 transition-colors duration-150">
 				<span className='text-white "font-semibold'>{item.filename}</span>
 				<span className='text-gray-400'>{dateTimeString}</span>
 			</Link>;
@@ -35,7 +38,9 @@ const UploadsList = ({uploads}: UploadListProps) => {
 
 	return (
 		<div className="flex flex-col items-center">
-			{renderedList}
+			{loaded ? 
+			renderedList : 
+			<UploadListItemsSkeleton />}
 		</div>
 	);
 };
