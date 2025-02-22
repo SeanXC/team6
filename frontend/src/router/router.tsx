b759612a695c2b8f91d8c8ddc6c92de60c5a40c0
ImDarkTom <darktomg21@gmail.com>
2025-02-22 12:28:27 +0000

    init for the 3rd time

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router';
+import Login from '@pages/Login';
+import UploadPage from '@/pages/Upload';
+
+function AppRouter(): React.ReactElement {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				<Route path='/upload' element={<UploadPage />} />
+				<Route path='*' element={<Navigate to='/' />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default AppRouter;
