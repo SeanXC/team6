b63bea7876b83c8e1c399d2c2b23fcdeb855d2f9
Vojtech Baudys <vojta@baudys.name>
2025-02-23 00:56:45 +0000

    default route

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate, BrowserRouter } from 'react-router';
 import Login from '@pages/Login';
-import UploadPage from '@/pages/Upload';
 import Dashboard from '@/pages/Dashboard';
 import SummaryPage from '@/pages/Summary';
 import UpdateProfile from '@/pages/UpdateProfile';
@@ -12,10 +11,9 @@ function AppRouter(): React.ReactElement {
 			<Routes>
 				<Route path='/login' element={<Login />} />
 				<Route path='/update-profile' element={<UpdateProfile/>}/>
-				<Route path='/upload' element={<UploadPage />} />
 				<Route path='/dashboard' element={<Dashboard />} />
 				<Route path='/summary/:id' element={<SummaryPage />} />
-				<Route path='*' element={<Navigate to='/' />} />
+				<Route path='*' element={<Navigate to='/login' />} />
 			</Routes>
 		</BrowserRouter>
 	);
